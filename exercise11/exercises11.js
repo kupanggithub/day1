9d659709ab60b5a2212f16cad32f619fe52aa9b1
kupanggithub <53647615+kupanggithub@users.noreply.github.com>
2019-08-22 11:38:08 +0700

    Add files via upload

---
+++ exercise11/exercises11.js
@@ -0,0 +1,21 @@
+function balikKata(kata) {
+  // you can only write your code here!
+  
+  var kataBalik = '';
+  
+  for (var i= kata.length -1; i >= 0; i --){
+    
+    kataBalik += kata[i];
+    
+  }
+  
+  return kataBalik;
+  
+}
+
+// TEST CASES
+console.log(balikKata('Hello World and Coders')); // sredoC dna dlroW olleH
+console.log(balikKata('John Doe')); // eoD nhoJ
+console.log(balikKata('I am a bookworm')); // mrowkoob a ma I
+console.log(balikKata('Coding is my hobby')); // ybboh ym si gnidoC
+console.log(balikKata('Super')); // repuS
